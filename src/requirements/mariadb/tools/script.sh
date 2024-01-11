aa91ac5878411640bbc9ad23f5b17b090363e6e0
Mlamarcq <mael.lamarcq0302@orange.fr>
2024-01-11 19:47:36 +0100

    In prgress

--- src/requirements/mariadb/tools/script.sh
+++ src/requirements/mariadb/tools/script.sh
@@ -1,13 +1,35 @@
-#!/bin/bash
+# #!/bin/bash
 
 
-service mariadb start;
+# service mariadb start;
 
-mariadb -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mariadb -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mariadb -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-mariadb -e "FLUSH PRIVILEGES;"
+# mariadb -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+# mariadb -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+# mariadb -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+# mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+# mariadb -e "FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
+# mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
+# exec mysqld_safe
+
+#!bin/bash
+
+service mysql start
+
+sleep 1
+
+mysql -u root -p$SQL_ROOT_PASSWORD
+
+mysql -u root -p$SQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+
+mysql -u root -p$SQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+
+mysql -u root -p$SQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+
+mysql -u root -p$SQL_ROOT_PASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+
+mysql -u root -p$SQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
+
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+
 exec mysqld_safe
